363e419f2ceb6210048dcb506c277b19d679b9dc
Remco Meeuwissen <hello@kalm.io>
2016-03-14 14:10:02 +0100

    Updated the index route to only take logged in and authorized users

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@
 
 const express = require('express');
 const router = new express.Router();
+const ensureAuthenticated = require('../middleware/ensureAuthenticated');
+const ensureAuthorized = require('../middleware/ensureAuthorized');
 
-router.get('/', (req, res) => {
+router.get('/', ensureAuthenticated, ensureAuthorized, (req, res) => {
   res.render('index', { user: req.user });
 });
 

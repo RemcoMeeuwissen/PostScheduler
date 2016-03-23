39052a8e15d5fd0280b507dbf5198001b17e59a5
Remco Meeuwissen <hello@kalm.io>
2016-03-23 22:57:31 +0100

    Update the schedule template and added code to store the post data in the database

--- routes/schedule.js
+++ routes/schedule.js
@@ -4,20 +4,43 @@ const express = require('express');
 const router = new express.Router();
 const ensureAuthenticated = require('../middleware/ensureAuthenticated');
 const ensureAuthorized = require('../middleware/ensureAuthorized');
+const Post = require('../models/post.js');
 
 router.get('/', ensureAuthenticated, ensureAuthorized, (req, res) => {
   res.render('schedule');
 });
 
 router.post('/', ensureAuthenticated, ensureAuthorized, (req, res) => {
-  console.log(req.body.inputTitle);
-  console.log(req.body.inputBody);
-  console.log(req.body.inputDate);
-  console.log(req.body.inputTime);
-  console.log(req.body.inputRepeats);
-  console.log(req.body.inputInterval);
+  const title = req.body.inputTitle;
+  const body = req.body.inputBody;
+  const time = new Date(new Date(req.body.inputDay).setHours(req.body.inputHour));
 
-  res.render('schedule');
+  let repeats = false;
+  if (req.body.inputRepeats === '1') repeats = true;
+
+  let interval = 0;
+  if (repeats) {
+    if (req.body.inputInterval === 'daily') interval = 60 * 60 * 24;
+    if (req.body.inputInterval === 'weekly') interval = 60 * 60 * 24 * 7;
+  }
+
+  const newPost = new Post({
+    title,
+    body,
+    time,
+    repeats,
+    interval,
+  });
+
+  newPost.save((err, post) => {
+    if (err || post === null) {
+      req.flash('info', 'Could not add the post, please try again later.');
+    } else {
+      req.flash('info', 'The post has been scheduled.');
+    }
+
+    return res.redirect('/');
+  });
 });
 
 module.exports = router;

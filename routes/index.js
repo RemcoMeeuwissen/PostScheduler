d754e30be53d06cc4f6c39edc76bd1957a634f76
Remco Meeuwissen <hello@kalm.io>
2016-03-24 15:45:40 +0100

    The homepage now properly shows the scheduled posts

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,22 @@ const express = require('express');
 const router = new express.Router();
 const ensureAuthenticated = require('../middleware/ensureAuthenticated');
 const ensureAuthorized = require('../middleware/ensureAuthorized');
+const Post = require('../models/post.js');
 
 router.get('/', ensureAuthenticated, ensureAuthorized, (req, res) => {
-  res.render('index', {
-    user: req.user,
-    messages: req.flash('info'),
+  Post.find({}).sort('time').exec((err, posts) => {
+    if (err) {
+      res.render('index', {
+        message: 'Could not get the scheduled posts, please try again later.',
+        error: {},
+        messages: req.flash('info'),
+      });
+    } else {
+      res.render('index', {
+        posts,
+        messages: req.flash('info'),
+      });
+    }
   });
 });
 
